0fd4b74eb7a30f7c16c3e3e7637f607dcd9d0012
Jackie <zhuweiping@madxstudio.cn>
2016-11-20 10:20:32 +0800

    第一阶段：创建项目 Sunshine完成

---
+++ app/src/main/java/com/jackie/sunshine/app/MainActivity.java
@@ -0,0 +1,79 @@
+/*
+ *
+ *             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ *             $                                                   $
+ *             $                       _oo0oo_                     $
+ *             $                      o8888888o                    $
+ *             $                      88" . "88                    $
+ *             $                      (| -_- |)                    $
+ *             $                      0\  =  /0                    $
+ *             $                    ___/`-_-'\___                  $
+ *             $                  .' \\|     |$ '.                 $
+ *             $                 / \\|||  :  |||$ \                $
+ *             $                / _||||| -:- |||||- \              $
+ *             $               |   | \\\  -  $/ |   |              $
+ *             $               | \_|  ''\- -/''  |_/ |             $
+ *             $               \  .-\__  '-'  ___/-. /             $
+ *             $             ___'. .'  /-_._-\  `. .'___           $
+ *             $          ."" '<  `.___\_<|>_/___.' >' "".         $
+ *             $         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       $
+ *             $         \  \ `_.   \_ __\ /__ _/   .-` /  /       $
+ *             $     =====`-.____`.___ \_____/___.-`___.-'=====    $
+ *             $                       `=-_-='                     $
+ *             $     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   $
+ *             $                                                   $
+ *             $          Buddha Bless         Never Bug           $
+ *             $                                                   $
+ *             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ */
+
+package com.jackie.sunshine.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new
+                    PlaceholderFragment()).commit();
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        private ArrayAdapter<String> mForecastAdapter;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastArray = getContext().getResources().getStringArray(R.array.forecasts);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.list_forecast);
+
+            mForecastAdapter = new ArrayAdapter<>(getContext(), R.layout.list_item_forecast, R.id
+                    .tv_list_item_forecast, forecastArray);
+            listView.setAdapter(mForecastAdapter);
+            return rootView;
+        }
+    }
+}
