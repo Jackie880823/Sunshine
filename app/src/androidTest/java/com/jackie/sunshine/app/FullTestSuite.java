bf35b25399a9daa695e8e6b4c449f4db0fd373bd
Jackie <zhuweiping@madxstudio.cn>
2016-12-02 11:36:35 +0800

    Test weather.db

--- app/src/androidTest/java/com/jackie/sunshine/app/FullTestSuite.java
+++ app/src/androidTest/java/com/jackie/sunshine/app/FullTestSuite.java
@@ -1,4 +1,5 @@
 /*
+ *
  *             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
  *             $                                                   $
  *             $                       _oo0oo_                     $
@@ -24,70 +25,21 @@
  *             $          Buddha Bless         Never Bug           $
  *             $                                                   $
  *             $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
- *
- *  Copyright (C) 2014 The Android Open Source Project
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
  */
-
 package com.jackie.sunshine.app;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.test.suitebuilder.TestSuiteBuilder;
 
-public class MainActivity extends BaseActivity {
-    private static final String TAG = "MainActivity";
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate: ");
-        showHomeButton(false);
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
-        if (savedInstanceState == null) {
-            Fragment fragment = new ForecastFragment();
-            setFragment(fragment);
-        }
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart: ");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop: ");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy: ");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause: ");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume: ");
+    public FullTestSuite() {
+        super();
     }
 }
